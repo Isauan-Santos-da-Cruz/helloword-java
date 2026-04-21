617dd1ce65aee1f966bfc25b9517995631a416ba
Isauan <isauansantos012@mail.com>
2026-04-21 16:56:35 -0300

    if e else estudos

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+ public static void main(String[] args){
+
+   String nome = "isauan";
+   //nome =  "nicoli" ;
+
+   char controle = 'v';
+   System.out.println(nome);
+   if (controle == 'v'){
+      System.out.println("a condiçao é verdadeira");
+   }
+   else {
+      System.out.println("houve um erro no sistema");
+
+      }  
+   } 
+}
